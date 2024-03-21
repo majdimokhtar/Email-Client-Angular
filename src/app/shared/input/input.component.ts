5e04b500890d2a6e5d3d853f73dd0860ad1f968d
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2024-03-21 14:16:28 +0100

    custom input and fixing bugs

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
+  @Input() label: string;
+  @Input() control: FormControl;
+  @Input() inputType: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
   }
-
 }
