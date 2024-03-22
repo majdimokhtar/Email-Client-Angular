1dd7bcc1e9ade2f54e2a3e6a6b91608f5187d0d2
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2024-03-22 14:49:13 +0100

    sign in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AuthModule } from "./auth/auth.module"
+import { Component } from "@angular/core"
+import { AuthService } from "./auth/auth.service"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'emailclient';
+  signedin = false
+  constructor(private authservice: AuthService) {}
+  ngOnInit() {
+    this.authservice.signedin$.subscribe((signedin)=>{
+      this.signedin = signedin
+    })
+  }
 }
