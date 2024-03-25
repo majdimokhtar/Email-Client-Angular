b2a9386b2c08f0e3c2494af95e951a07089530fc
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2024-03-25 10:51:29 +0100

    modifying requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { AuthModule } from "./auth/auth.module"
-import { Component } from "@angular/core"
-import { AuthService } from "./auth/auth.service"
+import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  signedin = false
-  constructor(private authservice: AuthService) {}
+  signedin$: BehaviorSubject<boolean>;
+
+  constructor(private authService: AuthService) {
+    this.signedin$ = this.authService.signedin$;
+  }
+
   ngOnInit() {
-    this.authservice.signedin$.subscribe((signedin)=>{
-      this.signedin = signedin
-    })
+    this.authService.checkAuth().subscribe(() => {});
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,46 +1,55 @@
-import { Injectable } from "@angular/core"
-import { HttpClient } from "@angular/common/http"
-import { BehaviorSubject } from "rxjs"
-import { tap } from "rxjs/operators"
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 interface UsernameAvailableResponse {
-  available: boolean
+  available: boolean;
 }
 
 interface SignupCredentials {
-  username: string
-  password: string
-  passwordConfirmation: string
+  username: string;
+  password: string;
+  passwordConfirmation: string;
 }
+
 interface SignupResponse {
-  username: string
+  username: string;
 }
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root'
 })
 export class AuthService {
-  rootUrl = "https://api.angular-email.com"
-  signedin$ = new BehaviorSubject(false)
+  rootUrl = 'https://api.angular-email.com';
+  signedin$ = new BehaviorSubject(false);
 
   constructor(private http: HttpClient) {}
 
   usernameAvailable(username: string) {
     return this.http.post<UsernameAvailableResponse>(
-      this.rootUrl + "/auth/username",
+      `${this.rootUrl}/auth/username`,
       {
-        username,
+        username
       }
-    )
+    );
   }
 
   signup(credentials: SignupCredentials) {
     return this.http
-      .post<SignupResponse>(this.rootUrl + "/auth/signup", credentials)
+      .post<SignupResponse>(`${this.rootUrl}/auth/signup`, credentials)
       .pipe(
         tap(() => {
-          this.signedin$.next(true)
+          this.signedin$.next(true);
         })
-      )
+      );
+  }
+
+  checkAuth() {
+    return this.http.get(`${this.rootUrl}/auth/signedin`).pipe(
+      tap(response => {
+        console.log(response);
+      })
+    );
   }
 }
