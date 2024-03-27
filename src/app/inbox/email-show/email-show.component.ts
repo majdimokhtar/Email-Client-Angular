38e0485d156984def0b9b654820894b238c83134
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2024-03-27 11:36:39 +0100

    err handling with resolvers

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
-import { EmailService } from '../email.service';
 import { Email } from '../email';
 
 @Component({
@@ -12,20 +10,12 @@ import { Email } from '../email';
 export class EmailShowComponent implements OnInit {
   email: Email;
 
-  constructor(
-    private route: ActivatedRoute,
-    private emailService: EmailService
-  ) {}
-
-  ngOnInit() {
-    this.route.params
-      .pipe(
-        switchMap(({ id }) => {
-          return this.emailService.getEmail(id);
-        })
-      )
-      .subscribe(email => {
-        this.email = email;
-      });
+  constructor(private route: ActivatedRoute) {
+    this.email = route.snapshot.data.email;
+    this.route.data.subscribe(({ email }) => {
+      this.email = email;
+    });
   }
+
+  ngOnInit() {}
 }
